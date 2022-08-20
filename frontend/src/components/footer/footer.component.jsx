620aed8e0947b6e678d354db4ac58423613bc323
Srishti2128 <srishtishukla66721@gmail.com>
2022-08-20 23:08:27 +0530

    Update name

--- frontend/src/components/footer/footer.component.jsx
+++ frontend/src/components/footer/footer.component.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; Pro Shop</Col>
+          <Col className='text-center py-3'>Copyright &copy; DigiDen</Col>
         </Row>
       </Container>
     </footer>
